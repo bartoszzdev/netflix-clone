e4e427c73ad31b862d5cb0d4e90a973801df7755
Claudinei <claudineicampos95@gmail.com>
2022-02-28 17:41:02 -0300

    Adicionando responsividade ao componente services

--- src/assets/Services/Services.js
+++ src/assets/Services/Services.js
@@ -34,10 +34,12 @@ function Services() {
                         <img className='card-img-02' src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" alt='Cell phone' />
 
                         <div className='card-animation-02'>
-                            <img className='movie-card-animation' alt='Stranger things' src='https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png' />
-                            <div className='card-animation-text'>
-                                <h4>Stranger Things</h4>
-                                <h5>Download em andamento...</h5>
+                            <div className='card-movie-02'>
+                                <img className='movie-card-animation' alt='Stranger things' src='https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png' />
+                                <div className='card-animation-text'>
+                                    <h4>Stranger Things</h4>
+                                    <h5>Download em andamento...</h5>
+                                </div>
                             </div>
                         </div>
                     </div>
